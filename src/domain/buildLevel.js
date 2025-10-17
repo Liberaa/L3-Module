2414898963207b1a8f33aef01db3e969d54c8409
Lukas <lukas.soderlund.fanta@gmail.com>
2025-10-17 13:50:00 +0200

    HotKeys Implemented

--- src/domain/buildLevel.js
+++ src/domain/buildLevel.js
@@ -1,11 +1,26 @@
 import { Obstacle, Coin } from '../../node_modules/learn2dgame-js/dist/learn2dgame-js.js'
 
-
 export function applyLevel(game, level) {
     for (const element of level.elements) {
         if (element.type === 'coin') {
             new Coin({ id: element.id, positionX: element.x, positionY: element.y })
             continue
         }
+        const deadly = element.type === 'deadly'
+        const disappearOnLand = element.type === 'vanishingPlatform'
+        const velocityX = element.velocityX || 0
+        const velocityY = element.velocityY || 0
+        new Obstacle({
+            id: element.id,
+            positionX: element.x,
+            positionY: element.y,
+            width: element.width,
+            height: element.height,
+            color: element.color,
+            deadly,
+            disappearOnLand,
+            velocityX,
+            velocityY
+        })
     }
 }
--- src/ui/Hotkeys.js
+++ src/ui/Hotkeys.js
@@ -1,8 +1,21 @@
 export class Hotkeys {
-    #handlers = new Map()
-    attach(){
-
-    }
-
-    detach(){}
+  #handlers = new Map()
+  bind(key, handler, options) {
+    const lower = key.toLowerCase()
+    this.#handlers.set(lower, { handler, once: !!options?.once })
+  }
+  attach() {
+    document.addEventListener('keydown', this.#dispatch)
+  }
+  detach() {
+    document.removeEventListener('keydown', this.#dispatch)
+    this.#handlers.clear()
+  }
+  #dispatch = (e) => {
+    const k = e.key.toLowerCase()
+    const entry = this.#handlers.get(k)
+    if (!entry) return
+    entry.handler()
+    if (entry.once) this.#handlers.delete(k)
+  }
 }
