819cebb56dddc7c05dbe4184eb87848e0cfd6ed8
lukas söderlund <ls224ec@student.lnu.se>
2025-10-21 10:43:20 +0200

    Private fields, smaller functions in Music class, hide data

--- src/app/Music.js
+++ src/app/Music.js
@@ -1,14 +1,45 @@
 export class Music {
   #audio
+
   constructor({ src, loop, volume }) {
-    this.#audio = new Audio(src)
-    this.#audio.loop = loop
-    this.#audio.volume = volume
+    this.#audio = this.#createAudioElement(src, loop, volume)
   }
+
   play() {
-    if (this.#audio.paused) {
-      this.#audio.currentTime = 0
-      this.#audio.play()
+    if (this.#isPaused()) {
+      this.#restartFromBeginning()
     }
+  }
+
+  pause() {
+    if (!this.#isPaused()) {
+      this.#audio.pause()
+    }
+  }
+
+  stop() {
+    this.pause()
+    this.#resetToStart()
+  }
+
+  #createAudioElement(src, loop, volume) {
+    const audio = new Audio(src)
+    audio.loop = loop
+    audio.volume = volume
+    return audio
+  }
+
+  #isPaused() {
+    return this.#audio.paused
+  }
+
+  #restartFromBeginning() {
+    this.#resetToStart()
+    this.#audio.play()
+  }
+
+  #resetToStart() {
+    const BEGINNING = 0
+    this.#audio.currentTime = BEGINNING
   }
 }
